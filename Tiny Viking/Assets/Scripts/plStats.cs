0293befcba6ea3ca790d32a602e344ead724e5c6
Dan Ford <Dan@Fordsville.co.uk>
2015-08-25 12:38:06 +0100

    Project Organisation

--- Tiny Viking/Assets/Scripts/plStats.cs
+++ Tiny Viking/Assets/Scripts/plStats.cs
@@ -5,9 +5,14 @@ public class plStats : MonoBehaviour {
 
 	public string Name;
 	public float Speed;
+	public int MaxHealth;
+	public int CurHealth;
+
 	void Start () {
 		Name = "player";
 		Speed = 1.0f;
+		MaxHealth = 10;
+		CurHealth = 10;
 	}
 
 }
